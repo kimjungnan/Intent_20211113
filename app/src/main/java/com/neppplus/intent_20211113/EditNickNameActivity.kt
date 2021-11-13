43d51b444e16522c121ebbb38767605336032e95
kjr <kjr0814@hotmail.com>
2021-11-13 16:21:18 +0900

    변경확인   - 입력값 변수 저장   - 결과 데이터 첨부용   - Result_OK   - Finish

--- app/src/main/java/com/neppplus/intent_20211113/EditNickNameActivity.kt
+++ app/src/main/java/com/neppplus/intent_20211113/EditNickNameActivity.kt
@@ -1,11 +1,26 @@
 package com.neppplus.intent_20211113
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nick_name.*
 
 class EditNickNameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_nick_name)
+
+        btnOK.setOnClickListener {
+
+            val inputNickName = textNickName.text.toString()
+
+            val resultIntent = Intent()
+            resultIntent.putExtra("nick", inputNickName)
+
+            setResult(RESULT_OK, resultIntent)
+
+            finish()
+
+        }
     }
 }
