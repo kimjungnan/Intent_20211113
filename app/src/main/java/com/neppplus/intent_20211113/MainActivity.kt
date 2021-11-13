53c17ceeff4efa5339750f407a406df05e9adcfc
kjr <kjr0814@hotmail.com>
2021-11-13 15:12:06 +0900

    복귀 기능 - Intent 연습문제 - 화면 이동시 이전 화면이 계속 누적됨

--- app/src/main/java/com/neppplus/intent_20211113/MainActivity.kt
+++ app/src/main/java/com/neppplus/intent_20211113/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.neppplus.intent_20211113
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnMoveToOther.setOnClickListener {
+
+//            OtherActivity로 이동
+
+            val myIntent = Intent(this, OtherActivity::class.java)
+            startActivity(myIntent)
+
+
+        }
+
     }
 }
---
+++ app/src/main/java/com/neppplus/intent_20211113/OtherActivity.kt
@@ -0,0 +1,19 @@
+package com.neppplus.intent_20211113
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_other.*
+
+class OtherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_other)
+
+        btnReturnMain.setOnClickListener {
+            val myIntent2 = Intent(this, MainActivity::class.java)
+            startActivity(myIntent2)
+
+        }
+    }
+}
