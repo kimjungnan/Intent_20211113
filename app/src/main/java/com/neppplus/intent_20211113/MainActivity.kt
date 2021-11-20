1ecea8da7de85be48b6c62b56debb0b88c996007
kjr <kjr0814@hotmail.com>
2021-11-20 10:25:31 +0900

    메인화면으로 복귀 - onActivitity 함수 작성   - 닉네임 받아온건지 확인   - ok 누른건지   - data Intent에 담긴 새 닉네임 추출 / UI반영

--- app/src/main/java/com/neppplus/intent_20211113/MainActivity.kt
+++ app/src/main/java/com/neppplus/intent_20211113/MainActivity.kt
@@ -52,4 +52,29 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+//        request_Code : 어떤 것을 가지러 다녕녹너지 알려주는 숫자가 기록됨
+
+        if (requestCode == REQ_FOR_NICKNAME) {
+//            닉네임을 가지러 다녀왔을때 실행됨
+
+//            resultCode : 확인(ok) / 취소(cancle) 중 어떤것을 눌렀는지 알려줌
+
+            if(resultCode == RESULT_OK ){
+//                닉네임을 가지러 가서 확인버튼(ok)을 누른게 맞을때 실행되는 코드
+
+//                data : 이전화면에서 담아준 resultIntent를 들고 있는 역할
+
+                val newNickName = data?.getStringExtra("nick")
+
+                txtNickname.text = newNickName
+
+            }
+        }
+
+    }
+
+
 }
